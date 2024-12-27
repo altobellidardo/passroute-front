3d7f845950554e01d56e82401f5b364e19e4be1a
Dardo Altobelli <altobellidardo@gmail.com>
2024-12-27 11:27:38 -0300

    feat: separate footer to a component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className='bg-purple-900 py-3 text-center'>
+      <p>&copy; 2024 PassRoute. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
